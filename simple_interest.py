b54cfc11004ee88c3fd7ef9e518c2384b28b7ca7
akshadadarade3-create <akshadadarade3@gmail.com>
2026-09-25 16:33:27 +0530

    Delete simple_interest.py

--- simple_interest.py
+++
@@ -1,5 +0,0 @@
-principal =float(input("Enter Principal value:")) 
-rate = float(input("Enter Rate:"))
-time = float(input("Enter Time:"))
-Simple_Interest = (principal*rate*time)/100
-print("SI:",Simple_Interest)
